3b0807ea63efe917d5c49e1b8f72fe907b618e2a
Roman Matviitsiv <matviitsivroman@gmail.com>
2022-07-15 03:42:50 +0300

    Remake face detection. Now it is based on pretrained deep learning models

--- object_recognition/object_recognition/camTest.py
+++ object_recognition/object_recognition/camTest.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def capture_camera(cam_port: int) -> None:
@@ -7,28 +8,59 @@ def capture_camera(cam_port: int) -> None:
 
     cam = cv2.VideoCapture(cam_port)
 
-    # initialize the face recognizer (default face haar cascade)
-    face_cascade = cv2.CascadeClassifier(
-        "cascades/haarcascade_frontalface_default.xml"
-    )
+    prototxt_path = "weights/deploy.prototxt.txt"
+    model_path = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+    model = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
 
     # Capture video from camera and split it into frames while cam is working
     while cam.isOpened():
         ret, frame = cam.read()
 
-        # Convering each frame to gray color
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray_frame, 2, 5)
+        h, w = frame.shape[:2]
+        # preprocess the image: resize and performs mean subtraction
+        blob = cv2.dnn.blobFromImage(
+            frame, 1.0, (300, 300), (104.0, 177.0, 123.0)
+        )
 
-        # for every face, draw a blue rectangle
-        for x, y, width, height in faces:
-            cv2.rectangle(
-                frame,
-                (x, y),
-                (x + width, y + height),
-                color=(255, 0, 0),
-                thickness=2,
-            )
+        # set the image into the input of the neural network
+        model.setInput(blob)
+
+        # perform inference and get the result
+        output = np.squeeze(model.forward())
+        font_scale = 1.0
+        for i in range(0, output.shape[0]):
+            # get the confidence
+            confidence = output[i, 2]
+
+            # if confidence is above 50%, then draw the surrounding box
+            if confidence > 0.5:
+
+                # get the surrounding box cordinates and upscale them
+                # to original image
+                box = output[i, 3:7] * np.array([w, h, w, h])
+
+                # convert to integers
+                start_x, start_y, end_x, end_y = box.astype(np.int)
+
+                # draw the rectangle surrounding the face
+                cv2.rectangle(
+                    frame,
+                    (start_x, start_y),
+                    (end_x, end_y),
+                    color=(255, 0, 0),
+                    thickness=2,
+                )
+
+                # draw text as well
+                cv2.putText(
+                    frame,
+                    f"{confidence*100:.2f}%",
+                    (start_x, start_y - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    font_scale,
+                    (255, 0, 0),
+                    2,
+                )
 
         cv2.imshow("Camera", frame)
 
